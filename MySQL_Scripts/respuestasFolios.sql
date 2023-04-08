d1cd9011b1817c520c248472acfcfd33878b10b1
Eduardo Zentella <ed.zent.2000@gmail.com>
2023-04-08 13:42:22 -0600

    Actualizacion de Scripts_MySQL_Eduardo

--- MySQL_Scripts/respuestasFolios.sql
+++ MySQL_Scripts/respuestasFolios.sql
@@ -3,15 +3,20 @@ USE ecoa_diner;
 
 CREATE TABLE IF NOT EXISTS  RespuestasFolios
 (
+    ClaveRespuesta 	INT NOT NULL AUTO_INCREMENT,
     ClavePregunta	VARCHAR(5) NOT NULL,
     Folio    		INT NOT NULL,
     ClaveEncuesta   VARCHAR(5) NOT NULL,
+    TipoPregunta    VARCHAR(5) NOT NULL,
+    Nomina			VARCHAR(9),
     TipoResp		VARCHAR(20),
     Evaluacion		INT,
     Comentario		VARCHAR(100),
-    PRIMARY KEY (ClavePregunta,FOLIO),
+    PRIMARY KEY (ClaveRespuesta),
     FOREIGN KEY (ClavePregunta) REFERENCES Pregunta(ClavePregunta),
-     FOREIGN KEY (ClaveEncuesta) REFERENCES Encuesta(ClaveEncuesta)
+    FOREIGN KEY (ClaveEncuesta) REFERENCES Encuesta(ClaveEncuesta),
+    FOREIGN KEY (Nomina) REFERENCES Profesor(Nomina),
+    CHECK ((TipoPregunta = 'RET' AND Nomina IS NOT NULL) OR (TipoPregunta = 'REC' AND Nomina IS NULL))
 );
 
 DESCRIBE RespuestasFolios;
