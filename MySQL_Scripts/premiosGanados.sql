e4fbe710f10ff2a111d0c2966da86a09b2f8dd04
Eduardo Zentella <ed.zent.2000@gmail.com>
2023-04-11 17:15:31 -0600

    Modificacion de Queries EncuestasAlumnos

--- MySQL_Scripts/premiosGanados.sql
+++ MySQL_Scripts/premiosGanados.sql
@@ -4,12 +4,12 @@ Use ecoa_diner;
 CREATE TABLE IF NOT EXISTS  PremiosGanados
 (
     NumReg			varchar(5),
-    matricula		varchar(9),
-    PremioID		varchar(5),
+    Matricula		varchar(9),
+    ClavePremio		varchar(5),
     FechaGanado		date,
-    PRIMARY KEY (NumReg,matricula,PremioID),
+    PRIMARY KEY (NumReg,Matricula,ClavePremio),
     FOREIGN KEY (matricula) references Alumno(matricula),
-    FOREIGN KEY (PremioID) references Premio(PremioID)
+    FOREIGN KEY (ClavePremio) references Premio(ClavePremio)
 );
 
 DESCRIBE PremiosGanados;
